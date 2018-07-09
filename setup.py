597b1161857902e0d38767292dc829cbb527fe0b
skrtu <3kthegiant@gmail.com>
2018-07-08 22:46:24 -0400

    fixing this for pip10
    need to add this for pip10

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
-from pip.req import parse_requirements
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 install_reqs = parse_requirements('requirements.txt', session=False)
 reqs = [str(ir.req) for ir in install_reqs]
